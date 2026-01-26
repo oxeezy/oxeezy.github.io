d46cbce60cd7e1f359310351a96a0f47ebf19f46
oxeezy <145017825+oxeezy@users.noreply.github.com>
2026-01-26 20:23:48 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-    window.addEventListener("scroll", function(){
-            var header = document.querySelector("header");
-            if ((scrollY > 900 && scrollY <= 5600)) {
-        header.classList.add("sticky");
-    } else {
-        header.classList.remove("sticky");
-    }
-   
-    });
